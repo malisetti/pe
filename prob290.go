fe979e3c6d01b86e47020232ac6edb0cd84e7178
Seshachalam Malisetti <seshachalam@outlook.com>
2015-08-25 12:41:03 +0530

    why problems

--- prob290.go
+++
@@ -1,9 +0,0 @@
-package main
-
-/**
-https://projecteuler.net/problem=290
-
-Digital Signature
-
-How many integers 0 ≤ n < 1018 have the property that the sum of the digits of n equals the sum of digits of 137n?
-**/
